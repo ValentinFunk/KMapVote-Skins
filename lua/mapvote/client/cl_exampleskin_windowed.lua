eb9c34c489e2ad76e223f45ad426a97f10a4e579
Valentin Funk <funk.valentin@gmail.com>
2015-03-21 13:26:43 +0100

    Update cl_exampleskin_windowed.lua

--- lua/mapvote/client/cl_exampleskin_windowed.lua
+++ lua/mapvote/client/cl_exampleskin_windowed.lua
@@ -55,21 +55,8 @@ function SKIN:LayoutGMVoteFrame( panel )
 end
 
 function SKIN:LayoutMapVoteFrame( panel )
-	panel:Center( )
+	panel.shouldCenter = true
 	panel.targetHeight = 800
-	function panel:PerformLayout( )
-		local y = 0
-		self:SetTall( 10000 )
-		self.mapPanels:InvalidateLayout( true )
-		for k, v in pairs( self:GetChildren( ) ) do
-			local x, _y = v:GetPos( )
-			_y = _y + v:GetTall( )
-			if _y > y then
-				y = _y
-			end
-		end
-		self:SetTall( y + 10 )
-	end
 	
 	if MAPVOTE.AllowClose then
 		local closeButton = vgui.Create('DButton', panel)
